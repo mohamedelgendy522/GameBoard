48f40b77de5441a25dbe2003cc39269e9f0e6c12
Mohamed Mahmoud Ahmed Mahmoud ELGendy <mm6920@o365.fayoum.edu.eg>
2025-11-13 22:29:10 +0200

    Enhance win condition checks for non-zero values
    Added checks to ensure no zero values in winning conditions.

--- Numerical_Tic_Tac_Toe.cpp
+++ Numerical_Tic_Tac_Toe.cpp
@@ -36,13 +36,13 @@ bool Numerical_Board::is_win(Player<int>* player) {
     
     // Check rows and columns
     for (int i = 0; i < rows; ++i) {
-        if ((board[i][0] + board[i][1] + board[i][2]) == 15) return true;
-        if ((board[0][i] + board[1][i] + board[2][i]) == 15) return true;
+        if ((board[i][0] + board[i][1] + board[i][2]) == 15 && board[i][0]!=0 && board[i][1]!=0 && board[i][2]!=0) return true;
+        if ((board[0][i] + board[1][i] + board[2][i]) == 15 && board[0][i]!=0 && board[1][i]!=0 && board[2][i]!=0) return true;
     }
 
     // diagonals
-    if ((board[0][0] + board[1][1] + board[2][2]) == 15) return true;
-    if ((board[0][2] + board[1][1] + board[2][0]) == 15) return true;
+    if ((board[0][0] + board[1][1] + board[2][2]) == 15 && board[0][0]!=0 && board[1][1]!=0 && board[2][2]!=0) return true;
+    if ((board[0][2] + board[1][1] + board[2][0]) == 15 && board[0][2]!=0 && board[1][1]!=0 && board[2][0]!=0) return true;
 
     return false;
 }
